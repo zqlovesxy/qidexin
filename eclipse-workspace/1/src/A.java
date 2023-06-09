f427d1c24f422f7f3010789cfb160b37cf83ff60
zqlovesxy <zhuqiyyl1128@icloud.com>
2023-06-09 17:31:09 +0800

    1

---
+++ eclipse-workspace/1/src/A.java
@@ -0,0 +1,11 @@
+
+public class A {byte b1 = 10, b2 = 20 ;
+short p = 200 ;
+int n = 500 ;
+long q = 100 ;
+float x = 2.5f ;
+double y = 5.25 ;
+char c = 60, ce = 'e', cg = 'g' ;
+byte b = 10 ;
+
+}
